a3c3af042a964dd0cdb2c0885a212c68a0ab1df6
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-25 14:36:10 +0900

    feat: Add a example of factorial.

--- projects/hello_calc/src/lib.rs
+++ projects/hello_calc/src/lib.rs
@@ -9,6 +9,26 @@ fn bad_add(a: i32, b: i32) -> i32 {
     a - b
 }
 
+pub fn factorial_recursive_if_else(n: u32) -> u32 {
+    if n < 2 {
+        1
+    } else {
+        n * factorial_recursive_if_else(n - 1)
+    }
+}
+
+pub fn factorial_recursive_use_match(num: u64) -> u64 {
+    match num {
+        0 | 1 => 1,
+        _ => factorial_recursive_use_match(num - 1) * num,
+    }
+}
+
+pub fn factorial_iterative(num: u64) -> u64 {
+    // (1..=num).product()
+    (1..=num).fold(1, |acc, v| acc * v)
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -25,5 +45,23 @@ mod tests {
         // This assert would fire and test will fail.
         // Please note, that private functions can be tested too!
         assert_eq!(bad_add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_factorial_recursive_if_else() {
+        assert_eq!(factorial_recursive_if_else(0), 1);
+        assert_eq!(factorial_recursive_if_else(4), 24);
+    }
+
+    #[test]
+    fn test_factorial_recursive_use_match() {
+        assert_eq!(factorial_recursive_use_match(0), 1);
+        assert_eq!(factorial_recursive_use_match(4), 24);
+    }
+
+    #[test]
+    fn test_factorial_iterative() {
+        assert_eq!(factorial_iterative(0), 1);
+        assert_eq!(factorial_iterative(4), 24);
     }
 }
