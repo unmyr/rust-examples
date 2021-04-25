708f71edf8745d7d06aed07830d31c8795c79cbc
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-25 15:59:16 +0900

    feat(guess_game): Handling Invalid Input.

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -8,22 +8,31 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // Handling Invalid Input
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                // Quitting After a Correct Guess
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
