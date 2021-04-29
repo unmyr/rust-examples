d8f5dbfe33a27e7db084acd7b61cc536e3621d65
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-29 20:08:43 +0900

    feat(restaurant): Exposing Paths with the pub Keyword.

--- tutorials/restaurant/src/lib.rs
+++ tutorials/restaurant/src/lib.rs
@@ -1,17 +1,15 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
+}
 
-    mod serving {
-        fn take_order() {}
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
 
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
 
 #[cfg(test)]
