0f194bbcd14cc877b5adac19823dd6211273d7fc
unmyr <56982541+unmyr@users.noreply.github.com>
2021-04-29 20:02:18 +0900

    feat: Add the first code.

--- tutorials/restaurant/src/lib.rs
+++ tutorials/restaurant/src/lib.rs
@@ -1,3 +1,19 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
