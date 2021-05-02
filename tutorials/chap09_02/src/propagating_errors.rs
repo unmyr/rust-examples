fc117095d2e1488a60121b37169907007df9c792
unmyr <56982541+unmyr@users.noreply.github.com>
2021-05-03 04:14:43 +0900

    feat(chap09-02): Add a example of propagating errors.

---
+++ tutorials/chap09_02/src/propagating_errors.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::io;
+use std::io::Read;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn main() {
+    match read_username_from_file() {
+        Ok(str) => println!("str is {}", str),
+        Err(error) => panic!("Problem opening the file: {:?}", error)
+    };
+}
+
