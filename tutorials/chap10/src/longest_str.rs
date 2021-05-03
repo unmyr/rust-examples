03b25476bff8e1af90932816b0e0095a643e9a3f
unmyr <56982541+unmyr@users.noreply.github.com>
2021-05-03 23:58:01 +0900

    wip(chap10-03): Add a example of lifetime.

---
+++ tutorials/chap10/src/longest_str.rs
@@ -0,0 +1,16 @@
+fn longest<'a>(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    // 最長の文字列は、{}です
+    println!("The longest string is {}", result);
+}
