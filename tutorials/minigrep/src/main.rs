ff5361d90a5dac1f0cd7fb1a85122a6e9d081d97
unmyr <56982541+unmyr@users.noreply.github.com>
2021-05-04 17:54:49 +0900

    wip(chap12-03): Splitting Code into a Library Crate.

--- tutorials/minigrep/src/main.rs
+++ tutorials/minigrep/src/main.rs
@@ -1,7 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,31 +20,5 @@ fn main() {
         println!("Application error: {}", e);
 
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
