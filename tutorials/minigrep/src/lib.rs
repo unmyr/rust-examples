5bfd6beab4dfc76465de01716079248b063f2825
unmyr <56982541+unmyr@users.noreply.github.com>
2021-05-04 18:32:39 +0900

    feat: Storing the lines that match so we can return them.

--- tutorials/minigrep/src/lib.rs
+++ tutorials/minigrep/src/lib.rs
@@ -1,13 +1,13 @@
 use std::error::Error;
 use std::fs;
 
-struct Config {
-    query: String,
-    filename: String,
+pub struct Config {
+    pub query: String,
+    pub filename: String,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
@@ -19,10 +19,36 @@ impl Config {
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
-
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
