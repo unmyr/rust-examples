2e64ade309ea694841e8642b922b44102c34eed7
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-22 19:19:14 +0900

    wip(tutorials/chap13): Listing 13-2

--- tutorials/chap13/src/main.rs
+++ tutorials/chap13/src/main.rs
@@ -7,4 +7,11 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     intensity
 }
 
-fn main() {}
+fn generate_workout(intensity: u32, random_number: u32) {}
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+}
