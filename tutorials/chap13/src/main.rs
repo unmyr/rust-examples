1b2be33fcab32c73a5ff47bc485c9d83b3f9d2f5
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-22 19:56:22 +0900

    wip(tutorials/chap13): Listing 13-3

--- tutorials/chap13/src/main.rs
+++ tutorials/chap13/src/main.rs
@@ -7,7 +7,27 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     intensity
 }
 
-fn generate_workout(intensity: u32, random_number: u32) {}
+fn generate_workout(intensity: u32, random_number: u32) {
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            simulated_expensive_calculation(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            simulated_expensive_calculation(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                simulated_expensive_calculation(intensity)
+            );
+        }
+    }
+}
 
 fn main() {
     let simulated_user_specified_value = 10;
