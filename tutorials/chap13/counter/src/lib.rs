2f0601f0b95e98609148fd1636f46503dc718655
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-23 14:46:23 +0900

    feat(factorial): Add example of our own iterator.

---
+++ tutorials/chap13/counter/src/lib.rs
@@ -0,0 +1,61 @@
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    #[allow(dead_code)]
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn using_zip() {
+        let mut iter = Counter::new()
+            .zip(Counter::new().skip(1));
+        assert_eq!(iter.next(), Some((1,2)));
+        assert_eq!(iter.next(), Some((2,3)));
+        assert_eq!(iter.next(), Some((3,4)));
+        assert_eq!(iter.next(), Some((4,5)));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn using_other_iterator_trait_methods() {
+        let sum: u32 = Counter::new()
+            .zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)     // [1*2, 2*3, 3*4, 4*5]
+            .filter(|x| x % 3 == 0)  // [2*3, 3*4]
+            .sum();
+        assert_eq!(18, sum);
+    }
+}
