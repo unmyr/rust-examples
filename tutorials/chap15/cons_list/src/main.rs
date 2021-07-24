1173c18698c108414175e67549202c3f5b1da26b
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-24 21:08:51 +0900

    feat(tutorials/chap15): Listing 15-5: Definition of List that uses Box<T> in order to have a known size

--- tutorials/chap15/cons_list/src/main.rs
+++ tutorials/chap15/cons_list/src/main.rs
@@ -1,6 +1,11 @@
 enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
-fn main() {}
+use crate::List::{Cons, Nil};
+
+fn main() {
+    #[allow(unused_variables)]
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
