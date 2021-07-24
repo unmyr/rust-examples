deb07eb15f34950f72a8b31aacde22a188336da6
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-24 21:44:51 +0900

    feat(tutorials/chap15): Listing 15-10: Implementing Deref on MyBox<T>.

--- tutorials/chap15/deref_example/src/main.rs
+++ tutorials/chap15/deref_example/src/main.rs
@@ -1,10 +1,25 @@
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 struct MyBox<T>(T);
 
 impl<T> MyBox<T> {
-    #[allow(dead_code)]
     fn new(x: T) -> MyBox<T> {
         MyBox(x)
     }
 }
 
-fn main() {}
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
