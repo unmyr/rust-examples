583e841a3cb650cddec3e311c866a10f1ac20899
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-26 01:05:55 +0900

    wip(tutorials/chap20): Listing 20-13: Implementing ThreadPool::new to panic if size is zero

--- tutorials/chap20/multi-threaded-server/src/lib.rs
+++ tutorials/chap20/multi-threaded-server/src/lib.rs
@@ -1,8 +1,17 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
     #[allow(unused_variables)]
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
         ThreadPool
     }
 
