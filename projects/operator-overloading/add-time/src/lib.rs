bf7192c7a4473372cb95aaf0f0e30caf20fd8991
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-31 03:06:35 +0900

    feat(projects/operator-overloading): Add a example.

---
+++ projects/operator-overloading/add-time/src/lib.rs
@@ -0,0 +1,35 @@
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
+struct Time {
+    sec: i32,
+    min: i32,
+    hour: i32,
+}
+
+impl Add for Time {
+    type Output = Time;
+
+    fn add(self, other: Time) -> Time {
+        let sum_sec = self.sec + other.sec;
+        let sum_min = self.min + other.min  + sum_sec / 60;
+        let sum_hour = self.hour + other.hour;
+        Time {
+            sec: sum_sec % 60,
+            min: sum_min % 60,
+            hour: sum_hour + sum_min / 60,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_add() {
+        use crate::Time;
+        assert_eq!(
+            Time { hour: 0, min: 59, sec: 59 } + Time { hour: 0, min: 0, sec: 2 },
+            Time { hour: 1, min: 0, sec: 1 }
+        );
+    }
+}
