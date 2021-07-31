7146675db8735315918dca062dd3f8ceaa41ebfb
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-31 23:27:42 +0900

    feat(ch17-03-oo-design-patterns): Listing 17-12: Definition of a Post struct and a new function that creates a new Post instance, a State trait, and a Draft struct.

--- tutorials/chap17/ch17-03-oo-design-patterns/src/lib.rs
+++ tutorials/chap17/ch17-03-oo-design-patterns/src/lib.rs
@@ -1,3 +1,23 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+}
+
+trait State {}
+
+struct Draft {}
+
+impl State for Draft {}
+
 #[cfg(test)]
 mod tests {
     #[test]
