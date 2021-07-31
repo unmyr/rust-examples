9d9ad8e93926f05d50a344187eab6b39b7951c32
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-31 11:11:02 +0900

    feat(ch16-02-message-passing): Add a example of mpsc.

---
+++ tutorials/chap16/ch16-02-message-passing/src/main.rs
@@ -0,0 +1,15 @@
+use std::thread;
+use std::sync::mpsc;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    // 値は{}です
+    println!("Got: {}", received);
+}
