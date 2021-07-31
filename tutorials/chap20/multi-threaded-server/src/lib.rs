4770c8b3cd5e85bde96b51aa157ecca086755b66
unmyr <56982541+unmyr@users.noreply.github.com>
2021-08-01 04:08:17 +0900

    feat(multi-threaded-server): Listing 20-24: Sending Message::Terminate to the workers before calling join on each worker thread.

--- tutorials/chap20/multi-threaded-server/src/lib.rs
+++ tutorials/chap20/multi-threaded-server/src/lib.rs
@@ -52,6 +52,12 @@ impl ThreadPool {
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
         println!("Shutting down all workers.");
 
         for worker in &mut self.workers {
