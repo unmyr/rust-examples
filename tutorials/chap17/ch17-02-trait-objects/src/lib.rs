1fabe8a7b6f928283d721df64c2a00b24947a9ee
unmyr <56982541+unmyr@users.noreply.github.com>
2021-07-31 22:42:22 +0900

    feat(ch17-02-trait-objects): Listing 17-5: A run method on Screen that calls the draw method on each component

--- tutorials/chap17/ch17-02-trait-objects/src/lib.rs
+++ tutorials/chap17/ch17-02-trait-objects/src/lib.rs
@@ -6,6 +6,14 @@ pub struct Screen {
     pub components: Vec<Box<dyn Draw>>,
 }
 
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
