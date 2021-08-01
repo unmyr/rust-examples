dd7c3e9e1b61eed539e835ce3dfe35ce7c1d7fc9
unmyr <56982541+unmyr@users.noreply.github.com>
2021-08-01 21:45:48 +0900

    feat(mutable-borrow): Add an example of getting a closure.

--- projects/closures/mutable-borrow/src/lib.rs
+++ projects/closures/mutable-borrow/src/lib.rs
@@ -19,6 +19,44 @@ where
     f();
 }
 
+// Rust 1.26 later.
+/// # Examples
+/// 
+/// ```
+/// use mutable_borrow::get_counter;
+/// 
+/// let mut inc = get_counter(3);
+/// let count = inc();
+/// assert_eq!(count, 4);
+/// ```
+pub fn get_counter(initial_value: i32) -> impl FnMut() -> i32 {
+    let mut x = initial_value;
+    move || {
+        x += 1;
+        x
+    }
+}
+
+// Rust 1.26 later.
+/// # Examples
+/// 
+/// ```
+/// use mutable_borrow::get_counter_old_style;
+/// 
+/// let mut inc = get_counter_old_style(3);
+/// let count = inc();
+/// assert_eq!(count, 4);
+/// ```
+pub fn get_counter_old_style(initial_value: i32) -> Box<dyn FnMut() -> i32> {
+    let mut x = initial_value;
+    Box::new(
+        move || {
+            x += 1;
+            x
+        }
+    )
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -56,5 +94,26 @@ mod tests {
 
         call_fn_once(fnmut_closure);
         assert_eq!(mut_val, "mut-new");
+    }
+
+    #[test]
+    fn test_get_fnmut_closure() {
+        // Rust 1.26 later.
+        use crate::get_counter;
+
+        let mut inc = get_counter(3);
+        let _count = inc();
+        let count = inc();
+        assert_eq!(count, 5);
+    }
+
+    #[test]
+    fn test_get_fnmut_closure_old_style() {
+        use crate::get_counter_old_style;
+
+        let mut inc = get_counter_old_style(3);
+        let _count = inc();
+        let count = inc();
+        assert_eq!(count, 5);
     }
 }
