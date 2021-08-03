b4c9a2a6e367bd6597ece52afdd05603e2855922
unmyr <56982541+unmyr@users.noreply.github.com>
2021-08-03 22:13:57 +0900

    refactor(get-closures-mut): Improve examples.

--- projects/closures/get-closures-mut/src/lib.rs
+++ projects/closures/get-closures-mut/src/lib.rs
@@ -2,17 +2,42 @@
 /// # Examples
 /// 
 /// ```
-/// use get_closures_mut::get_counter;
+/// use get_closures_mut::get_push_char_move;
 /// 
-/// let mut inc = get_counter(3);
-/// let count = inc();
-/// assert_eq!(count, 4);
+/// let initial_str = String::from("hello");
+/// let str_to_append = String::from("!");
+/// let mut push_em = get_push_char_move(initial_str, str_to_append);
+/// 
+/// let new_str = push_em();
+/// assert_eq!(new_str, "hello!");
 /// ```
-pub fn get_counter(initial_value: i32) -> impl FnMut() -> i32 {
-    let mut x = initial_value;
+pub fn get_push_char_move(
+    initial_str: String, str_to_append: String
+) -> impl FnMut() -> String {
+    let mut s = initial_str;
+    let a = str_to_append;
     move || {
-        x += 1;
-        x
+        s.push_str(&a);
+        s.clone()
+    }
+}
+
+/// # Examples
+/// 
+/// ```
+/// use get_closures_mut::get_push_char_borrow;
+/// 
+/// let mut push_em = get_push_char_borrow("hello", "!");
+/// let count = push_em();
+/// assert_eq!(count, 6);
+/// ```
+pub fn get_push_char_borrow<'a>(
+    initial_str: &str, str_to_append: &'a str
+) -> impl 'a + FnMut() -> usize {
+    let mut s = String::from(initial_str);
+    move || {
+        s.push_str(str_to_append);
+        s.len()
     }
 }
 
@@ -40,12 +65,32 @@ mod tests {
     #[test]
     fn test_get_fnmut_closure() {
         // Rust 1.26 later.
-        use crate::get_counter;
+        use crate::get_push_char_move as get_push_char;
 
-        let mut inc = get_counter(3);
-        let _count = inc();
-        let count = inc();
-        assert_eq!(count, 5);
+        let initial_str = String::from("hello");
+        let mut push_em = get_push_char(initial_str, String::from("!"));
+
+        let new_str = push_em();
+        assert_eq!(new_str, "hello!");
+
+        let new_str = push_em();
+        assert_eq!(new_str, "hello!!");
+    }
+
+    #[test]
+    fn test_get_push_char_borrow() {
+        // Rust 1.26 later.
+        use crate::get_push_char_borrow as get_push_char;
+
+        let initial_str = String::from("hello");
+        let mut push_em = get_push_char(&initial_str, "!");
+        let count = push_em();
+        assert_eq!(count, 6);
+        assert_eq!(initial_str, "hello");
+
+        let count = push_em();
+        assert_eq!(count, 7);
+        assert_eq!(initial_str, "hello");
     }
 
     #[test]
