52c0d6a5784ab23107f1a6a3119b417ab3e3da6c
unmyr <56982541+unmyr@users.noreply.github.com>
2021-08-25 00:21:22 +0900

    test(fs/file-rw): Add example of BufReader.

--- projects/fs/file-rw/src/lib.rs
+++ projects/fs/file-rw/src/lib.rs
@@ -21,9 +21,40 @@ pub fn read_from_file<P: AsRef<Path>>(path: P) -> Result<(usize, String), String
     Ok((read_size, contents))
 }
 
+pub fn buf_read_from_file<P: AsRef<Path>>(path: P) -> Result<(usize, String), String> {
+    let mut f = match File::open(&path) {
+        Ok(file) => std::io::BufReader::new(file),
+        Err(error) => {
+            return Err(format!("Problem opening the file: path={:?} error={:?}", path.as_ref(), error))
+        },
+    };
+
+    let mut contents = String::new();
+    let read_size = match f.read_to_string(&mut contents) {
+        Ok(read_size) => read_size,
+        Err(error) => {
+            return Err(format!("Problem reading the file: path={:?} error={:?}", path.as_ref(), error))
+        },
+    };
+    Ok((read_size, contents))
+}
+
 pub fn write_to_file<P: AsRef<Path>>(path: P, data: &[u8]) -> usize{
     let mut f = match OpenOptions::new().write(true).truncate(true).create(true).open(&path) {
         Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+    let write_size = match f.write(data) {
+        Ok(write_size) => write_size,
+        Err(error) => panic!("Problem writing the file: {:?}", error),
+    };
+    write_size
+}
+
+pub fn buf_write_to_file<P: AsRef<Path>>(path: P, data: &[u8]) -> usize{
+    let mut f = match OpenOptions::new().write(true).truncate(true).create(true).open(&path) {
+        Ok(file) => std::io::BufWriter::new(file),
         Err(error) => panic!("Problem opening the file: {:?}", error),
     };
 
@@ -58,6 +89,43 @@ mod tests {
         let write_size = write_to_file(path, b"hello");
         assert_eq!(write_size, 5);
         let (read_size, contents) = read_from_file(path).unwrap();
+        assert_eq!(read_size, 5);
+        assert_eq!(contents, "hello");
+
+        std::fs::remove_file(path).unwrap();
+    }
+
+    #[test]
+    fn test_buffer_reader() {
+        use std::fs::{OpenOptions};
+        use std::io::BufReader;
+        use std::io::Read;
+
+        let path = std::path::Path::new("test_write_string.txt");
+
+        let write_size = write_to_file(path, b"hello");
+        assert_eq!(write_size, 5);
+
+        let mut file = BufReader::new(
+            OpenOptions::new().read(true).open(&path).unwrap()
+        );
+        let mut contents = String::new(); 
+        let read_size = file.read_to_string(&mut contents).unwrap();
+        assert_eq!(read_size, 5);
+        assert_eq!(contents, "hello");
+
+        std::fs::remove_file(path).unwrap();
+     }
+
+    #[test]
+    fn test_buf_read_from_file() {
+        use crate::{buf_write_to_file, buf_read_from_file};
+
+        let path = std::path::Path::new("test_buf_read_from_file.txt");
+
+        let write_size = buf_write_to_file(path, b"hello");
+        assert_eq!(write_size, 5);
+        let (read_size, contents) = buf_read_from_file(path).unwrap();
         assert_eq!(read_size, 5);
         assert_eq!(contents, "hello");
 
