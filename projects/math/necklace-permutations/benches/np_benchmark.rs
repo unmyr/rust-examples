e29b699e646b20eb791bca084690a3abb72f7bd3
unmyr <56982541+unmyr@users.noreply.github.com>
2021-12-29 22:03:36 +0900

    perf(necklace-permutations): Add criterion benchmarks.

---
+++ projects/math/necklace-permutations/benches/np_benchmark.rs
@@ -0,0 +1,20 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use necklace_permutations::necklace_perm_with_filter;
+use necklace_permutations::necklace_perm;
+
+pub fn bm_np(c: &mut Criterion) {
+    c.bench_function("necklace_perm_with_filter(11)", |b| b.iter(
+        || necklace_perm_with_filter(black_box(
+            vec![1, 2, 4, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        )))
+    );
+
+    c.bench_function("necklace_perm_iterative(11)", |b| b.iter(
+        || necklace_perm(black_box(
+            vec![1, 2, 4, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        )))
+    );
+}
+
+criterion_group!(benches, bm_np);
+criterion_main!(benches);
