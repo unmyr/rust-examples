06ec4c26c01cdff4fea990e73ebbc5c73ebab049
unmyr <56982541+unmyr@users.noreply.github.com>
2022-01-02 18:39:33 +0900

    feat(math/itertools-examples): Add examples of `permutations`.

---
+++ projects/math/itertools-examples/src/main.rs
@@ -0,0 +1,14 @@
+use itertools::Itertools;
+
+fn main() { 
+    let n = 11;
+    println!("P({},{})", n, n);
+    let mut i = 0;
+    let max_iter = (1..=n).fold(1, |acc, v| acc * v);
+    for perm in (1..=n).permutations(n) {
+        if (i+1) % (max_iter/3) == 0 {
+            println!("[{:10}]{:?}", i, perm);
+        }
+        i += 1;
+    }
+}
