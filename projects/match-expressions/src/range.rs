e78faf7645cfef457579ee0e659b8af61c4c045d
unmyr <56982541+unmyr@users.noreply.github.com>
2022-01-08 18:32:49 +0900

    feat(match-expressions): Add examples.

---
+++ projects/match-expressions/src/range.rs
@@ -0,0 +1,39 @@
+pub enum Message {
+    Hello {id: i32}
+}
+
+/// # Examples
+/// 
+/// ```
+/// use match_expressions::range::{Message, get_message_str};
+/// get_message_str(&(Message::Hello { id: 5 }));
+/// ```
+pub fn get_message_str(msg: &Message) -> String {
+    let message = match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => format!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } => {
+            format!("Found an id in another range")
+        }
+        Message::Hello { id } => format!("Found some other id: {}", id),
+    };
+    message
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_get_message_str() {
+        use crate::range::{Message, get_message_str};
+
+        let msg = Message::Hello { id: 5 };
+        assert_eq!(get_message_str(&msg), "Found an id in range: 5");
+
+        let msg = Message::Hello { id: 12 };
+        assert_eq!(get_message_str(&msg), "Found an id in another range");
+
+        let msg = Message::Hello { id: 1 };
+        assert_eq!(get_message_str(&msg), "Found some other id: 1");
+    }
+}
---
+++ projects/match-expressions/src/tuple.rs
@@ -0,0 +1,43 @@
+/// # Examples
+/// 
+/// ```
+/// use match_expressions::tuple::get_message_str;
+/// assert_eq!(
+///     get_message_str(Some("hello"), Some(3)),
+///     "msg=hello, code=3"
+/// );
+/// ```
+pub fn get_message_str(a: Option<&str>, b: Option<u8>) -> String {
+    match (a, b) {
+        (Some(msg), Some(code)) => {
+            format!("msg={}, code={}", msg, code)
+        },
+        (_, None) => format!("msg=*, code=Nil"),
+        (None, Some(code)) => format!("msg=Nil, code={}", code),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_get_message_str() {
+        use crate::tuple::{get_message_str};
+
+        assert_eq!(
+            get_message_str(Some("hello"), Some(3)),
+            "msg=hello, code=3"
+        );
+        assert_eq!(
+            get_message_str(Some("hello"), None),
+            "msg=*, code=Nil"
+        );
+        assert_eq!(
+            get_message_str(None, Some(1)),
+            "msg=Nil, code=1"
+        );
+        assert_eq!(
+            get_message_str(None, None),
+            "msg=*, code=Nil"
+        );
+    }
+}
