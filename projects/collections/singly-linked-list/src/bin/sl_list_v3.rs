7d3b8bd777514b7a6be073791c7b40522fdc03a3
unmyr <56982541+unmyr@users.noreply.github.com>
2022-01-08 19:13:48 +0900

    refactor(singly-linked-list): Initialize using `Default::default()`.

--- projects/collections/singly-linked-list/src/bin/sl_list_v3.rs
+++ projects/collections/singly-linked-list/src/bin/sl_list_v3.rs
@@ -1,7 +1,7 @@
 use list::v3::SinglyLinkedList;
 
 fn main() {
-    let mut list = SinglyLinkedList::new();
+    let mut list: SinglyLinkedList<u8> = Default::default();
     list.push_back(1);
     list.push_back(2);
     list.push_back(3);
