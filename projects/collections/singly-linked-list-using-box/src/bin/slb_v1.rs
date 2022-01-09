92943f18a483e2a5678b0d68aab06c19669ea6b8
unmyr <56982541+unmyr@users.noreply.github.com>
2022-01-10 00:13:30 +0900

    feat(singly-linked-list-using-box): Add an example.

---
+++ projects/collections/singly-linked-list-using-box/src/bin/slb_v1.rs
@@ -0,0 +1,21 @@
+use singly_linked_list_using_box::v1::SinglyLinkedList;
+
+fn main() {
+    let mut list: SinglyLinkedList<u8> = Default::default();
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    println!("{}", list);
+
+    assert_eq!(list.pop_front(), Some(1));
+    assert_eq!(list.pop_front(), Some(2));
+    assert_eq!(list.pop_front(), Some(3));
+    assert_eq!(list.pop_front(), None);
+    println!("{}", list);
+
+    list.push_back(1);
+    list.push_back(2);
+    assert_eq!(list.pop_back(), Some(2));
+    assert_eq!(list.pop_back(), Some(1));
+    assert_eq!(list.pop_back(), None);
+}
---
+++ projects/collections/singly-linked-list-using-box/src/v1.rs
@@ -0,0 +1,184 @@
+#[derive(Debug)]
+pub struct ListNode<T> {
+    pub value: T,
+    pub next: Option<Box<ListNode<T>>>,
+}
+
+#[derive(Default, Debug)]
+pub struct SinglyLinkedList<T> {
+    pub head: Option<Box<ListNode<T>>>,
+}
+
+impl<T: Clone + std::fmt::Debug> ListNode<T> {
+    pub fn new(v: T) -> ListNode<T> {
+        ListNode { value: v, next: None }
+    }
+
+    fn push_back(&mut self, v: T) {
+        match &mut self.next {
+            None => {
+                self.next = Some(Box::new(ListNode::new(v)))
+            },
+            Some(ref mut next) => next.push_back(v),
+        }
+    }
+
+    // fn pop_back(&mut self) -> Option<T> {
+    //     match &mut self.next {
+    //         None => {
+    //             self.next = Some(Box::new(ListNode::new(v)))
+    //         },
+    //         Some(ref mut next) => next.push_back(v),
+    //     }
+    // }
+
+    pub fn list(& self) {
+        println!("{:?}", self.value);
+        match &self.next {
+            None => {},
+            Some(next) => next.list(),
+        }
+    }
+}
+
+impl<T: std::fmt::Debug + std::clone::Clone> SinglyLinkedList<T> {
+    /// # Examples
+    ///
+    /// ```
+    /// use singly_linked_list_using_box::v1::SinglyLinkedList;
+    /// let mut list: SinglyLinkedList<u8> = Default::default();
+    /// list.push_back(1);
+    /// list.push_back(2);
+    /// ```
+    pub fn push_back(&mut self, v: T) {
+        match &mut self.head {
+            Some(head_ref) => head_ref.push_back(v),
+            None => {
+                let node_new = ListNode::new(v);
+                self.head = Some(Box::new(node_new));
+            }
+        }
+    }
+
+    /// # Examples
+    ///
+    /// ```
+    /// use singly_linked_list_using_box::v1::SinglyLinkedList;
+    /// let mut list: SinglyLinkedList<u8> = Default::default();
+    /// list.push_back(1);
+    /// list.push_back(2);
+    /// assert_eq!(list.pop_back(), Some(2));
+    /// assert_eq!(list.pop_back(), Some(1));
+    /// assert_eq!(list.pop_back(), None);
+    /// ```
+    pub fn pop_back(&mut self) -> Option<T> {
+        use std::mem::replace;
+        let cur = replace(&mut self.head, None);
+        cur.as_ref()?;
+
+        let mut cur = cur.unwrap(); // safe because of the check above
+        if cur.next.is_none() {
+            return Some(cur.value);
+        }
+
+        let mut prev_next = &mut self.head;
+        while cur.next.is_some() {
+            // Take ownership of the next element
+            let n_next = replace(&mut cur.next, None).unwrap();
+
+            // Update the previous element's "next" field
+            *prev_next = Some(cur);
+
+            // Progress to the next element
+            cur = n_next;
+
+            // Progress our pointer to the previous element's "next" field
+            prev_next = &mut prev_next.as_mut().unwrap().next;
+        }
+
+        Some(cur.value)
+    }
+
+    /// # Examples
+    ///
+    /// ```
+    /// use singly_linked_list_using_box::v1::SinglyLinkedList;
+    /// let mut list: SinglyLinkedList<u8> = Default::default();
+    /// list.push_back(1);
+    /// list.push_back(2);
+    /// assert_eq!(list.pop_front(), Some(1));
+    /// assert_eq!(list.pop_front(), Some(2));
+    /// assert_eq!(list.pop_front(), None);
+    /// ```
+    pub fn pop_front(&mut self) -> Option<T> {
+        use std::mem::replace;
+        let cur = replace(&mut self.head, None);
+        cur.as_ref()?;
+
+        let cur = cur.unwrap();
+        self.head = cur.next;
+        Some(cur.value)
+    }
+}
+
+impl<T: std::fmt::Debug> std::fmt::Display for ListNode<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.next {
+            Some(ref next) => {
+                write!(f, "ListNode({:?}), {}", self.value, next)
+            },
+            None => write!(f, "ListNode({:?})", self.value)
+        }
+    }
+}
+
+impl<T: std::fmt::Debug> std::fmt::Display for SinglyLinkedList<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.head {
+            Some(ref head) => write!(f, "SinglyLinkedList[{}]", head),
+            None => write!(f, "SinglyLinkedList[]")
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SinglyLinkedList;
+
+    #[test]
+    fn test_pop_front() {
+        let mut list: SinglyLinkedList<u8> = Default::default();
+        assert_eq!(list.pop_front(), None);
+
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), None);
+
+        list.push_back(1);
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+
+    }
+
+    #[test]
+    fn test_pop_back() {
+        let mut list: SinglyLinkedList<u8> = Default::default();
+        assert_eq!(list.pop_back(), None);
+
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
+
+        list.push_back(1);
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
+    }
+}
