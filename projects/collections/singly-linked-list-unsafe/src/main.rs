30ff95310240906f5876f1a5083e09608ccb2edd
unmyr <56982541+unmyr@users.noreply.github.com>
2022-01-12 01:31:46 +0900

    feat(singly-linked-list-unsafe): Add an example of NonNull.

---
+++ projects/collections/singly-linked-list-unsafe/src/main.rs
@@ -0,0 +1,29 @@
+use singly_linked_list_unsafe::SinglyLinkedList;
+
+fn main() {
+    let mut list: SinglyLinkedList<u8> = Default::default();
+    list.push_back(1);
+    // list.push_back(2);
+    // let mut iter = list.iter();
+    // assert_eq!(iter.next(), Some(1));
+    // assert_eq!(list.pop_back(), Some(2));
+    // assert_eq!(iter.next(), None);  // NG: Accessing the freed memory!
+    // assert_eq!(list.iter().collect::<Vec<_>>(), vec![1]);
+
+    list.push_back(2);
+    list.push_back(3);
+    println!("{}", list);
+    assert_eq!(list.iter().collect::<Vec<_>>(), vec![1, 2, 3]);
+    for v in list.iter() {
+        println!("{:?}", v);
+    }
+    assert_eq!(list.pop_back(), Some(3));
+    assert_eq!(list.pop_back(), Some(2));
+    assert_eq!(list.pop_back(), Some(1));
+    assert_eq!(list.pop_back(), None);
+
+    list.push_back(1);
+    list.push_back(2);
+    assert_eq!(list.pop_front(), Some(1));
+    assert_eq!(list.iter().collect::<Vec<_>>(), vec![2]);
+}
