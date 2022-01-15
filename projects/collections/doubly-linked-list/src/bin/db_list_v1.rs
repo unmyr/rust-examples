4c85cde49541329a4a9c52780a5b75c7e19f04d5
unmyr <56982541+unmyr@users.noreply.github.com>
2022-01-16 01:52:05 +0900

    test(doubly-linked-list): Add a test code of `pop_back()`.

--- projects/collections/doubly-linked-list/src/bin/db_list_v1.rs
+++ projects/collections/doubly-linked-list/src/bin/db_list_v1.rs
@@ -6,4 +6,11 @@ fn main() {
     list.push_back(2);
     list.push_back(3);
     println!("{}", list);
+    assert_eq!(list.pop_back(), Some(3));
+    println!("{}", list);
+    assert_eq!(list.pop_back(), Some(2));
+    println!("{}", list);
+    assert_eq!(list.pop_back(), Some(1));
+    println!("{}", list);
+    assert_eq!(list.pop_back(), None);
 }
