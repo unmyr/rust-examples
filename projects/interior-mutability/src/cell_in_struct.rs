3f44af41a9aef56fdb75ea558b8e12631a89c613
unmyr <56982541+unmyr@users.noreply.github.com>
2022-01-15 17:59:15 +0900

    feat(interior-mutability): Improve an example.

--- projects/interior-mutability/src/cell_in_struct.rs
+++ projects/interior-mutability/src/cell_in_struct.rs
@@ -1,22 +1,46 @@
-use std::cell::Cell; 
+use std::cell::Cell;
 
 pub struct Point {
-    pub x: Cell<i32>,
-    pub y: i32
+    pub x: u8,
+    pub y: Option<Cell<u8>>,
 }
 
 impl Point {
+    pub fn new(x: u8, y: u8) -> Self {
+        Point { x: x, y: Some(Cell::new(y)) }
+    }
+
     /// # Examples
-    /// 
+    ///
     /// ```
     /// use interior_mutability::cell_in_struct::Point;
-    /// 
-    /// let point = Point { x: std::cell::Cell::new(1), y: 0 };
-    /// point.increment();
-    /// assert_eq!(point.x.get(), 2);
+    ///
+    /// let mut p: Point = Point::new(0, 1);
+    /// p.increment_both();
+    /// assert_eq!(p.x, 1);
+    /// assert_eq!(p.y.unwrap().get(), 2);
     /// ```
-    pub fn increment(&self) { // note: no mut again
-        self.x.set(self.x.get() + 1);
+    pub fn increment_both(&mut self) {  // require mutable instance
+        self.x += 1;
+        if let Some(ref y) = self.y {
+            y.set(y.get() + 1);
+        }
+    }
+
+    /// # Examples
+    ///
+    /// ```
+    /// use interior_mutability::cell_in_struct::Point;
+    ///
+    /// let p: Point = Point::new(0, 1);
+    /// p.increment_y();
+    /// assert_eq!(p.y.unwrap().get(), 2);
+    /// ```
+    pub fn increment_y(&self) {
+        // self.x += 1;
+        if let Some(ref y) = self.y {
+            y.set(y.get() + 1);
+        }
     }
 }
 
@@ -26,21 +50,33 @@ mod tests {
 
     #[test]
     fn test_im_direct() {
-        let point = Point {
-            x: Cell::new(1), // interior mutability using Cell
-            y: 0
-        };
-        point.x.set(2);
-        assert_eq!(point.x.get(), 2);
+        let p: Point = Point::new(0, 1);
+        if let Some(ref y) = p.y {
+            y.set(2);
+        }
+        assert_eq!(p.y.map(|y| y.get()), Some(2));
     }
 
     #[test]
-    fn test_im_method() {
-        let point = Point {
-            x: Cell::new(1), // interior mutability using Cell
-            y: 0
-        };
-        point.increment();
-        assert_eq!(point.x.get(), 2);
+    fn test_increment_both() {
+        let mut p: Point = Point::new(0, 1);
+        // let x_ref = &mut p.x;
+
+        p.increment_both();
+        // *x_ref += 1;
+
+        assert_eq!(p.x, 1);
+        assert_eq!(p.y.map(|y| y.get()), Some(2));
+    }
+
+    #[test]
+    fn test_increment_y() {
+        let p: Point = Point::new(0, 1);
+        let p_ref = &p;
+
+        p.increment_y();
+        p_ref.increment_y();
+
+        assert_eq!(p.y.map(|y| y.get()), Some(3));
     }
 }
