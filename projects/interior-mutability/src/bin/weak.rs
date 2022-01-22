c63502d45b439c45fab71eed6bc880829bca8841
unmyr <56982541+unmyr@users.noreply.github.com>
2022-01-23 04:52:18 +0900

    feat(interior-mutability): Add an example of `Rc::strong_count`.

--- projects/interior-mutability/src/bin/weak.rs
+++ projects/interior-mutability/src/bin/weak.rs
@@ -10,10 +10,16 @@ fn main() {
     bob = Rc::downgrade(&alice);
     assert_eq!(alice.borrow().clone(), Some(&1));
     assert_eq!(bob.upgrade().unwrap().borrow().clone(), Some(&1));
+    assert_eq!(1, Rc::strong_count(&alice));
+    assert_eq!(1, Rc::weak_count(&alice));
     println!("alice={:?}, bob={:?}", alice.borrow(), bob.upgrade());
 
     bob.upgrade().unwrap().replace(None);
     println!("alice={:?}, bob={:?}", alice.borrow(), bob.upgrade());
     assert_eq!(alice.borrow().clone(), None);
     assert_eq!(bob.upgrade().unwrap().borrow().clone(), None);
+
+    drop(alice);
+
+    assert_eq!(bob.upgrade(), None);
 }
