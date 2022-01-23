eee177adf302bdca7760b521fddd6ba0f3c4e65f
unmyr <56982541+unmyr@users.noreply.github.com>
2022-01-23 13:02:09 +0900

    feat(interior-mutability): Improve examples.

--- projects/interior-mutability/src/bin/try_unwrap_opt_rc_refcell.rs
+++ projects/interior-mutability/src/bin/try_unwrap_opt_rc_refcell.rs
@@ -1,55 +1,66 @@
 use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 
+struct Human {
+    alice: Option<Rc<RefCell<String>>>,
+    bob: Option<Weak<RefCell<String>>>,
+    mallory: Option<Rc<RefCell<String>>>,
+}
+
 fn main() {
     let alice: Option<Rc<RefCell<String>>>;
     let bob: Option<Weak<RefCell<String>>>;
-    let mut mallory: Option<Rc<RefCell<String>>>;
-    let s1 = String::from("hello");
+    let mallory: Option<Rc<RefCell<String>>>;
 
-    // The sum of the strong counts is 2.
+    let s1 = String::from("hello");
     alice = Some(Rc::new(RefCell::new(s1)));
     bob = Some(Rc::downgrade(alice.as_ref().unwrap()));
     mallory = Some(Rc::clone(alice.as_ref().unwrap()));
-    assert_eq!(2, Rc::strong_count(alice.as_ref().unwrap()));
-    assert_eq!(1, Rc::weak_count(alice.as_ref().unwrap()));
+
+    let mut h = Human { alice, bob, mallory };
+
+    // The sum of the strong counts is 2.
+    assert_eq!(2, Rc::strong_count(h.alice.as_ref().unwrap()));
+    assert_eq!(1, Rc::weak_count(h.alice.as_ref().unwrap()));
 
     // Update value.
-    mallory.as_ref().unwrap().borrow_mut().push_str(" world!");
+    h.mallory.as_ref().unwrap().borrow_mut().push_str(" world!");
     let s2 = String::from("hello world!");
-    assert_eq!(s2, alice.as_ref().unwrap().borrow().clone());
-    assert_eq!(s2, bob.as_ref().unwrap().upgrade().unwrap().borrow().clone());
-    assert_eq!(s2, mallory.as_ref().unwrap().borrow().clone());
+    assert_eq!(s2, h.alice.as_ref().unwrap().borrow().clone());
+    assert_eq!(s2, h.bob.as_ref().unwrap().upgrade().unwrap().borrow().clone());
+    assert_eq!(s2, h.mallory.as_ref().unwrap().borrow().clone());
     println!(
         "alice={:?}, bob={:?}",
-        alice.as_ref().unwrap().borrow(),
-        bob.as_ref().unwrap().upgrade()
+        h.alice.as_ref().unwrap().borrow(),
+        h.bob.as_ref().unwrap().upgrade()
     );
 
-    // `try_unwrap()` fails because the strong count is greater than 1.
-    let some_alice = alice.map(
-        |alice_rc| Rc::try_unwrap(alice_rc).unwrap_err()
-    );
-    assert_eq!(
-        Some(Rc::new(RefCell::new(s2.clone()))), some_alice
-    );
-    assert_eq!(2, Rc::strong_count(mallory.as_ref().unwrap()));
+    {
+        // `try_unwrap()` fails because the strong count is greater than 1.
+        let some_alice = h.alice.map(
+            |alice_rc| Rc::try_unwrap(alice_rc).unwrap_err()
+        );
+        assert_eq!(
+            Some(Rc::new(RefCell::new(s2.clone()))), some_alice
+        );
+        assert_eq!(2, Rc::strong_count(h.mallory.as_ref().unwrap()));
 
-    // Drop `alice` trapped in try_unwrap to decrement a strong count.
-    drop(some_alice);
-    assert_eq!(1, Rc::strong_count(mallory.as_ref().unwrap()));
+        // Drop `alice` trapped in try_unwrap to decrement a strong count.
+        drop(some_alice);
+        assert_eq!(1, Rc::strong_count(h.mallory.as_ref().unwrap()));
 
-    // Return the strong count to 2 for testing.
-    let alice = Some(Rc::clone(mallory.as_ref().unwrap()));
-    assert_eq!(2, Rc::strong_count(alice.as_ref().unwrap()));
+        // Return the strong count to 2 for testing.
+        h.alice = Some(Rc::clone(h.mallory.as_ref().unwrap()));
+        assert_eq!(2, Rc::strong_count(h.alice.as_ref().unwrap()));
+    }
 
     // Drop mallory to decrement one strong count.
-    mallory = None;
-    assert_eq!(None, mallory);
+    h.mallory = None;
+    assert_eq!(None, h.mallory);
 
     // The strong count is 1 now, so try `try_unwrap`.
-    assert_eq!(1, Rc::strong_count(alice.as_ref().unwrap()));
-    let some_alice = alice.map(
+    assert_eq!(1, Rc::strong_count(h.alice.as_ref().unwrap()));
+    let some_alice = h.alice.map(
         |alice_rc| Rc::try_unwrap(alice_rc).unwrap()
     );
     assert_eq!(Some(RefCell::new(s2)), some_alice);
--- projects/interior-mutability/src/bin/try_unwrap_rc_refcell.rs
+++ projects/interior-mutability/src/bin/try_unwrap_rc_refcell.rs
@@ -1,35 +1,61 @@
 use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 
+struct Human {
+    alice: Rc<RefCell<String>>,
+    bob: Weak<RefCell<String>>,
+    mallory: Rc<RefCell<String>>,
+}
+
 fn main() {
     let alice: Rc<RefCell<String>>;
     let bob: Weak<RefCell<String>>;
     let mallory: Rc<RefCell<String>>;
-    let s1 = String::from("hello");
 
-    // The sum of the strong counts is 2.
+    let s1 = String::from("hello");
     alice = Rc::new(RefCell::new(s1));
     bob = Rc::downgrade(&alice);
     mallory = Rc::clone(&alice);
-    assert_eq!(2, Rc::strong_count(&alice));
-    assert_eq!(1, Rc::weak_count(&alice));
+
+    let mut h = Human { alice, bob, mallory };
+
+    // The sum of the strong counts is 2.
+    assert_eq!(2, Rc::strong_count(&h.alice));
+    assert_eq!(1, Rc::weak_count(&h.alice));
 
     // Update value.
-    mallory.borrow_mut().push_str(" world!");
+    h.mallory.borrow_mut().push_str(" world!");
     let s2 = String::from("hello world!");
-    assert_eq!(s2, alice.borrow().clone());
-    assert_eq!(s2, bob.upgrade().unwrap().borrow().clone());
-    assert_eq!(s2, mallory.borrow().clone());
-    println!("alice={:?}, bob={:?}", alice.borrow(), bob.upgrade());
+    assert_eq!(s2, h.alice.borrow().clone());
+    assert_eq!(s2, h.bob.upgrade().unwrap().borrow().clone());
+    assert_eq!(s2, h.mallory.borrow().clone());
+    println!("alice={:?}, bob={:?}", h.alice.borrow(), h.bob.upgrade());
+
+    {
+        // `try_unwrap()` fails because the strong count is greater than 1.
+        let some_alice = Rc::try_unwrap(h.alice).unwrap_err();
+        assert_eq!(
+            Rc::new(RefCell::new(s2.clone())), some_alice
+        );
+        assert_eq!(2, Rc::strong_count(&h.mallory));
+
+        // Drop `alice` trapped in try_unwrap to decrement a strong count.
+        drop(some_alice);
+        assert_eq!(1, Rc::strong_count(&h.mallory));
+
+        // Return the strong count to 2 for testing.
+        h.alice = Rc::clone(&h.mallory);
+        assert_eq!(2, Rc::strong_count(&h.alice));
+    }
 
     // Drop mallory to decrement one strong count.
-    drop(mallory);
+    drop(h.mallory);
 
     // The strong count is 1 now, so try `try_unwrap`.
-    assert_eq!(1, Rc::strong_count(&alice));
+    assert_eq!(1, Rc::strong_count(&h.alice));
     assert_eq!(
         RefCell::new(s2),
-        Rc::try_unwrap(alice).unwrap()
+        Rc::try_unwrap(h.alice).unwrap()
     );
-    assert_eq!(None, bob.upgrade());
+    assert_eq!(None, h.bob.upgrade());
 }
