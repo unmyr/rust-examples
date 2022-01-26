ecf8303a72c247162029247fc344abf18434b475
unmyr <56982541+unmyr@users.noreply.github.com>
2022-01-26 22:04:21 +0900

    feat(option-and-result): Add examples of Option.

--- projects/option-and-result/src/lib.rs
+++ projects/option-and-result/src/lib.rs
@@ -1,7 +1,132 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_option() {
+    fn test_option_update_if_let_1() {
+        let mut o1: Option<String> = Some(String::from("Hello"));
+        assert_eq!(o1.is_some(), true);
+        if let Some(ref mut s1) = o1 {
+            // Update inner string.
+            s1.push_str(" world!");
+        }
+        assert_eq!(o1.as_deref(), Some("Hello world!"));
+
+        let mut o1: Option<String> = None;
+        assert_eq!(o1.is_none(), true);
+        if let Some(ref mut s1) = o1 {
+            // Update inner string.
+            s1.push_str(" world!");
+        }
+        assert_eq!(o1, None);
+    }
+
+    #[test]
+    fn test_option_update_if_let_2() {
+        let mut o1: Option<String> = Some(String::from("Hello"));
+        assert_eq!(o1.is_some(), true);
+        if let Some(s1) = &mut o1 {
+            // Update inner string.
+            s1.push_str(" world!");
+        }
+        assert_eq!(o1, Some(String::from("Hello world!")));
+
+        let mut o1: Option<String> = None;
+        assert_eq!(o1.is_none(), true);
+        if let Some(s1) = &mut o1 {
+            // Update inner string.
+            s1.push_str(" world!");
+        }
+        assert_eq!(o1, None);
+    }
+
+    #[test]
+    fn test_option_update_as_mut() {
+        let mut o1: Option<String> = Some(String::from("Hello"));
+        assert_eq!(o1.is_some(), true);
+
+        // Update inner string.
+        o1.as_mut().map(|s| s.push_str(" world!"));
+        assert_eq!(o1, Some(String::from("Hello world!")));
+
+        let mut o1: Option<String> = None;
+        assert_eq!(o1.is_none(), true);
+        o1.as_mut().map(|s| s.push_str(" world!"));
+        assert_eq!(o1, None);
+    }
+
+    #[test]
+    fn test_option_replace() {
+        let mut o1: Option<String> = Some(String::from("Hello"));
+        assert_eq!(o1.is_some(), true);
+        // Replace inner string.
+        let old_o1 = o1.replace(String::from("Hello world!"));
+        assert_eq!(o1, Some(String::from("Hello world!")));
+        assert_eq!(old_o1, Some(String::from("Hello")));
+
+        let mut o1: Option<String> = None;
+        assert_eq!(o1.is_none(), true);
+        let old_o1 = o1.replace(String::from("Hello world!"));
+        assert_eq!(o1, Some(String::from("Hello world!")));
+        assert_eq!(old_o1, None);
+    }
+
+    #[test]
+    fn test_option_refcell_cloned() {
+        use std::cell::RefCell;
+
+        let o1 = Some(RefCell::new(String::from("Hello")));
+        assert_eq!(o1.is_some(), true);
+        let o2: &Option<RefCell<String>> = &o1.as_ref().cloned();
+
+        // Update inner string.
+        o2.as_ref().map(|c| c.borrow_mut().push_str(" world!"));
+
+        assert_eq!(
+            o1.as_ref().unwrap().borrow().clone(),
+            String::from("Hello")
+        );
+        assert_eq!(
+            o2.as_ref().unwrap().borrow().clone(),
+            String::from("Hello world!")
+        );
+    }
+
+    #[test]
+    fn test_option_rc_refcell_cloned() {
+        use std::rc::Rc;
+        use std::cell::RefCell;
+
+        let mut o1 = Some(Rc::new(RefCell::new(String::from("Hello"))));
+        assert_eq!(o1.is_some(), true);
+        let o2 = o1.as_ref().cloned();
+        assert_eq!(Rc::strong_count(o1.as_ref().unwrap()), 2);
+
+        // Update inner string.
+        o2.as_ref().map(|c| c.borrow_mut().push_str(" world!"));
+
+        assert_eq!(
+            o1.as_ref().unwrap().borrow().clone(),
+            String::from("Hello world!")
+        );
+        assert_eq!(
+            o2.as_ref().unwrap().borrow().clone(),
+            String::from("Hello world!")
+        );
+
+        // `Rc` of o1 is alive.
+        assert_eq!(Rc::strong_count(o1.as_ref().unwrap()), 2);
+        let o3 = o1.take();
+        o1 = o3;
+        assert_eq!(Rc::strong_count(o1.as_ref().unwrap()), 2);
+
+        // drop o1
+        assert_eq!(Rc::strong_count(o1.as_ref().unwrap()), 2);
+        o1 = None;
+        assert!(o1.is_none());
+        assert_eq!(Rc::strong_count(o2.as_ref().unwrap()), 1);
+    }
+
+    #[test]
+    fn test_option_take() {
         let mut o1: Option<String> = Some(String::from("Hello"));
         assert_eq!(o1.is_some(), true);
         o1.as_ref().map(|s| assert_eq!(s, &String::from("Hello")));
