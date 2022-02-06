1a6235dc9c1fe500e3747d653680600749b136dc
unmyr <56982541+unmyr@users.noreply.github.com>
2022-02-06 11:35:05 +0900

    test(slist-box-enum-tuple): Add a command to test `push_front()`.

---
+++ projects/collections/list/slist-box-enum-tuple/src/bin/slist_box_enum_tuple_push_front.rs
@@ -0,0 +1,26 @@
+use slist_box_enum_tuple::SList;
+
+fn main() {
+    let mut list: SList<u8> = Default::default();
+    assert_eq!(format!("{:?}", list), "SList(Nil)");
+
+    list.push_front(3);
+    assert_eq!(
+        format!("{:?}", list),
+        "SList(3) -> SList(Nil)"
+    );
+
+    list.push_front(2);
+    assert_eq!(
+        format!("{:?}", list),
+        "SList(2) -> SList(3) -> SList(Nil)"
+    );
+
+    list.push_front(1);
+    assert_eq!(
+        format!("{:?}", list),
+        "SList(1) -> SList(2) -> SList(3) -> SList(Nil)"
+    );
+
+    println!("{list:?}");
+}
