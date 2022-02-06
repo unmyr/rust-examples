d37c1c89325a3e750b31e38942a693d458871be9
unmyr <56982541+unmyr@users.noreply.github.com>
2022-02-06 12:05:09 +0900

    test(slist-rc-enum): Improve a command to test `push_front()`.

--- projects/collections/list/slist-rc-enum/src/bin/slist_rc_enum_push_front.rs
+++ projects/collections/list/slist-rc-enum/src/bin/slist_rc_enum_push_front.rs
@@ -2,8 +2,25 @@ use slist_rc_enum::SList;
 
 fn main() {
     let mut list: SList<u8> = Default::default();
-    println!("{list:?}"); list.push_front(3);
-    println!("{list:?}"); list.push_front(2);
-    println!("{list:?}"); list.push_front(1);
+    assert_eq!(format!("{:?}", list), "SList(Nil)");
+
+    list.push_front(3);
+    assert_eq!(
+        format!("{:?}", list),
+        "SList(3) -> SList(Nil)"
+    );
+
+    list.push_front(2);
+    assert_eq!(
+        format!("{:?}", list),
+        "SList(2) -> SList(3) -> SList(Nil)"
+    );
+
+    list.push_front(1);
+    assert_eq!(
+        format!("{:?}", list),
+        "SList(1) -> SList(2) -> SList(3) -> SList(Nil)"
+    );
+
     println!("{list:?}");
 }
