82527e8aa3ad5abd3b9a03b6e0d191f632243716
unmyr <56982541+unmyr@users.noreply.github.com>
2022-02-06 12:00:04 +0900

    test(dlist-rc-opt-refcell): Improve a command to test `push_front()`.

--- projects/collections/list/dlist-rc-opt-refcell/src/bin/dlist_rc_opt_refcell_push_front.rs
+++ projects/collections/list/dlist-rc-opt-refcell/src/bin/dlist_rc_opt_refcell_push_front.rs
@@ -2,8 +2,25 @@ use dlist_rc_opt_refcell::DList;
 
 fn main() {
     let mut list: DList<u8> = Default::default();
-    println!("{list:?}"); list.push_front(3);
-    println!("{list:?}"); list.push_front(2);
-    println!("{list:?}"); list.push_front(1);
+    assert_eq!(format!("{:?}", list), "DList[]");
+
+    list.push_front(3);
+    assert_eq!(
+        format!("{:?}", list),
+        "DList[(3, prev:Nil, next:Nil)]"
+    );
+
+    list.push_front(2);
+    assert_eq!(
+        format!("{:?}", list),
+        "DList[(value:2, prev:Nil, next:3) -> (value:3, prev:2, next:Nil)]"
+    );
+
+    list.push_front(1);
+    assert_eq!(
+        format!("{:?}", list),
+        "DList[(value:1, prev:Nil, next:2) -> (value:2, prev:1, next:3) -> (value:3, prev:2, next:Nil)]"
+    );
+
     println!("{list:?}");
 }
