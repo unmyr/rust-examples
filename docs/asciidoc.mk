69820c6b79915e62650a4823a99268422b844cf9
unmyr <56982541+unmyr@users.noreply.github.com>
2022-08-11 02:26:53 +0900

    ci: Add GitHub Actions to convert AsciiDoc.

---
+++ docs/asciidoc.mk
@@ -0,0 +1,20 @@
+IMAGES_DIR=images
+CP_OPTS=-n -l
+ROUGE_STYLE=monokai_sublime
+
+STYLESHEET_FILE=golo.css
+STYLESHEET_DIR=$(BASE_DIR)/docs/stylesheets
+
+AD_OPTS_HTML5_COMMON=\
+--backend=html5 \
+-a stylesheet=$(STYLESHEET_FILE) -a stylesdir=$(STYLESHEET_DIR) \
+-a imagesdir=$(IMAGES_DIR)
+
+AD_OPTS_HTML5_ROUGE=\
+$(AD_OPTS_HTML5_COMMON) \
+-r asciidoctor-diagram \
+-a source-highlighter=rouge \
+-a rouge-style=$(ROUGE_STYLE)
+
+AD_OPTS_HTML5_STEM=\
+$(AD_OPTS_HTML5_COMMON)
