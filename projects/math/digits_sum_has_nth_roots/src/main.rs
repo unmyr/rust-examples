0170ef252ee4eb5a79549aeddc2269bb165a9bf6
unmyr <56982541+unmyr@users.noreply.github.com>
2024-02-12 03:22:37 +0900

    ci: Upgrade edition to 2021.

--- projects/math/digits_sum_has_nth_roots/src/main.rs
+++ projects/math/digits_sum_has_nth_roots/src/main.rs
@@ -1,9 +1,7 @@
-pub mod lib;
-
 fn main() {
     for n in 1000..10000 {
-        let base = lib::sum_of_digits(n);
-        let (accum, exponent) = lib::check_nth_roots(n, base);
+        let base = digits_sum_has_nth_roots::sum_of_digits(n);
+        let (accum, exponent) = digits_sum_has_nth_roots::check_nth_roots(n, base);
         if n == accum {
             println!("{} ({}^{})", n, base, exponent);
         }
