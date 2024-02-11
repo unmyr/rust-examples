34a7890498af10c117f49b4f9ac6438e433e975d
unmyr <56982541+unmyr@users.noreply.github.com>
2024-02-12 03:17:51 +0900

    refactor(actix-web/simple-server): Upgrade actix-web to 4.

--- projects/actix-web/simple-server/src/main.rs
+++ projects/actix-web/simple-server/src/main.rs
@@ -1,7 +1,8 @@
 use actix_web::{get, web, App, HttpServer, Responder};
 
 #[get("/{id}/{name}/index.html")]
-async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+async fn index(params: web::Path<(u32, String)>) -> impl Responder {
+    let (id, name) = params.into_inner();
     format!("Hello {}! id:{}", name, id)
 }
 
