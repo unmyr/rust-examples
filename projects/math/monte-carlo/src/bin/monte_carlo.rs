696d9649adbcd3b45b2376d7e8fb731547021f19
unmyr <56982541+unmyr@users.noreply.github.com>
2025-12-24 02:48:04 +0900

    feat(math/monte-carlo): Allow external RNG injection to improve test stability in Monte Carlo PI estimation.
    Note: This changes the function signature and is not backward-compatible.

--- projects/math/monte-carlo/src/bin/monte_carlo.rs
+++ projects/math/monte-carlo/src/bin/monte_carlo.rs
@@ -1,4 +1,3 @@
-
 use monte_carlo::calc_pi;
 use std::f64::consts::PI;
 
@@ -13,16 +12,31 @@ fn main() {
         u64::pow(10, 6),
         u64::pow(10, 7),
     ];
+    // let seed: [u8; 32] = [13; 32];
+    let mut rng: rand::rngs::StdRng = rand::SeedableRng::from_os_rng();
+    let results = num_trials
+        .iter()
+        .map(|n| (n, calc_pi(*n, &mut rng)))
+        .collect::<Vec<_>>();
 
-    let results = num_trials.iter().map(
-        |n| (n, calc_pi(*n))
-    ).collect::<Vec<_>>();
+    let sigma = |n_samples: u64| {
+        let p = std::f64::consts::FRAC_PI_4; // true probability
+        4.0 * (p * (1.0 - p) / (n_samples as f64)).sqrt()
+    };
 
     for pair in results {
         let (n, monte_carlo_pi) = pair;
         println!(
-            "trial={:10}, result={:.10}, error={:.10}",
-            n, monte_carlo_pi, (monte_carlo_pi - PI).abs()
+            "trial={:10}, result={:.10}, error={:.10}, 2 sigma={:.10}, within_95%_conf={}",
+            n,
+            monte_carlo_pi,
+            (monte_carlo_pi - PI).abs(),
+            2.0 * sigma(*n),
+            if (monte_carlo_pi - PI).abs() < 2.0 * sigma(*n) {
+                "PASS"
+            } else {
+                "FAIL"
+            }
         );
     }
 }
--- projects/math/monte-carlo/src/lib.rs
+++ projects/math/monte-carlo/src/lib.rs
@@ -1,19 +1,20 @@
+use rand::Rng;
 use std::f64::consts::PI;
-use rand::{Rng};
 
 /// # Examples
 /// ```
 /// use monte_carlo::calc_pi;
-/// assert_eq!((calc_pi(1000) - 3.141).abs() < 0.11, true);
+/// let seed: [u8; 32] = [13; 32];
+/// let mut rng: rand::rngs::StdRng = rand::SeedableRng::from_seed(seed);
+/// assert_eq!((calc_pi(1000, &mut rng) - 3.141).abs() < 0.11, true);
 /// ```
-pub fn calc_pi(n_samples: u64) -> f64 {
-    let mut rng = rand::rng();
+pub fn calc_pi(n_samples: u64, rng: &mut rand::rngs::StdRng) -> f64 {
     let mut inside_area_count = 0;
 
     // Area D: x^2 + y^2 < 1
     for _ in 1..=n_samples {
-        let x: f64 = rng.random_range(0.0 .. 1.0);
-        let y: f64 = rng.random_range(0.0 .. 1.0);
+        let x: f64 = rng.random_range(0.0..1.0);
+        let y: f64 = rng.random_range(0.0..1.0);
         // Point (x, y) in inside area D
         if x * x + y * y < 1. {
             inside_area_count += 1;
@@ -27,12 +28,21 @@ pub fn calc_pi(n_samples: u64) -> f64 {
 
 #[cfg(test)]
 mod tests {
+    use std::f64::consts::FRAC_PI_4;
+
     use super::*;
 
     #[test]
     fn test_calc_pi() {
+        let p = FRAC_PI_4; // true probability
         let n_samples = 1000;
-        let error = (calc_pi(n_samples) - PI).abs();
-        assert_eq!(error < 0.11, true);
+        let sigma = 4.0 * (p * (1.0 - p) / (n_samples as f64)).sqrt();
+
+        let seed: [u8; 32] = [13; 32];
+        let mut rng: rand::rngs::StdRng = rand::SeedableRng::from_seed(seed);
+        let error = (calc_pi(n_samples, &mut rng) - PI).abs();
+
+        // verify within 2 sigma (95% confidence interval)
+        assert_eq!(error < 2.0 * sigma, true);
     }
 }
