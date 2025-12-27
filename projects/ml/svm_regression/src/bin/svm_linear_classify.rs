87f88b643d0470ebfffa6bae863d157bcb6e8142
unmyr <56982541+unmyr@users.noreply.github.com>
2025-12-28 01:22:42 +0900

    feat(ml/svm_regression): add an example code for classifying linearly separable data using SVM

---
+++ projects/ml/svm_regression/src/bin/svm_linear_classify.rs
@@ -0,0 +1,38 @@
+use linfa::prelude::*;
+use linfa_preprocessing::linear_scaling::LinearScaler;
+use linfa_svm::Svm;
+use ndarray::array;
+
+fn main() {
+    let train_features = array![
+        [1.0, 1.0],
+        [2.0, 2.0],
+        [3.0, 3.0],
+        [4.0, 4.0],
+        [5.0, 5.0],
+        [6.0, 6.0],
+    ];
+    let train_labels: ndarray::ArrayBase<ndarray::OwnedRepr<bool>, ndarray::Dim<[usize; 1]>> =
+        array![false, false, false, true, true, true];
+
+    let train_dataset = DatasetBase::new(train_features, train_labels);
+    let train_scaler = LinearScaler::<_>::standard().fit(&train_dataset).unwrap();
+    let train_dataset_scaled = train_scaler.transform(train_dataset);
+    println!("== Scaled training data ==\n{:?}\n", train_dataset_scaled);
+    let model = Svm::<f64, bool>::params()
+        .pos_neg_weights(0.2, 0.2)
+        .fit(&train_dataset_scaled)
+        .expect("Training failed");
+
+    println!("== SVM Model ==\n{:?}\n", model);
+
+    // Prediction
+    let valid_data = array![[2., 2.], [3., 3.], [4., 4.], [5., 5.]];
+    let valid_labels_dummy = array![false, false, false, false]; // Dummy labels for dataset creation
+    let valid_dataset = DatasetBase::new(valid_data, valid_labels_dummy);
+    let valid_dataset_scaled = train_scaler.transform(valid_dataset);
+
+    let prediction = model.predict(&valid_dataset_scaled);
+
+    println!("Prediction results: {:?}", prediction);
+}
