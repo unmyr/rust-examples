7b9dca0404329c974808a41cda1f19ffbc1b3df4
unmyr <56982541+unmyr@users.noreply.github.com>
2026-01-02 08:56:20 +0900

    style(math/ndarray-example-mutmal): simplify array initialization and formatting in main function

--- projects/math/ndarray-example-mutmal/src/main.rs
+++ projects/math/ndarray-example-mutmal/src/main.rs
@@ -1,20 +1,11 @@
 use ndarray::arr2;
 
 fn main() {
-    let a = arr2(&[
-        [1, 2, 1],
-        [0, 1, 1]
-    ]);
-    let b = arr2(&[
-        [1, 0],
-        [0, 1],
-        [1, 1]
-    ]);
+    // Multiplying matrices
+    let a = arr2(&[[1, 2, 1], [0, 1, 1]]);
+    let b = arr2(&[[1, 0], [0, 1], [1, 1]]);
     let c = a.dot(&b);
     println!("{}", c);
-    println!(
-        "{} {}\n{} {}",
-        c[(0, 0)], c[(0, 1)], c[(1, 0)], c[(1, 1)]
-    );
+    println!("{} {}\n{} {}", c[(0, 0)], c[(0, 1)], c[(1, 0)], c[(1, 1)]);
     assert_eq!(c, arr2(&[[2, 3], [1, 2]]));
 }
