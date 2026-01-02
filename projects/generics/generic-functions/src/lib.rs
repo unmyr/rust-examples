75ecf8b31a949aa168aa1f670df5be8770bc7880
unmyr <56982541+unmyr@users.noreply.github.com>
2026-01-02 18:49:13 +0900

    feat(generics/generic-functions): add L2 norm and vector sum functions with tests

--- projects/generics/generic-functions/src/lib.rs
+++ projects/generics/generic-functions/src/lib.rs
@@ -1,3 +1,12 @@
+/// Computes the L2 norm (Euclidean distance) between two points (x, y).
+/// # Examples
+/// ```rust
+/// use generic_functions::l2_norm;
+/// assert!((l2_norm(3.0_f32, 4.0_f32) - 5.0).abs() < 1e-6);
+/// ```
+pub fn l2_norm<T: num_traits::Float>(x: T, y: T) -> T {
+    (x * x + y * y).sqrt()
+}
 
 /// Computes the sigmoid function for a given input `x`.
 /// # Examples
@@ -10,13 +19,18 @@ pub fn sigmoid<T: num_traits::Float>(x: T) -> T {
     T::one() / (T::one() + (-x).exp())
 }
 
-#[test]
-fn it_sigmoid() {
-    let x = 0.0f32;
-    let y = sigmoid(x);
-    assert!((y - 0.5).abs() < 1e-6);
-
-    let x = 0.0f64;
-    let y = sigmoid(x);
-    assert!((y - 0.5).abs() < 1e-6);
+/// Sum number in Vec
+/// NOTE: Copy is used so we can pass values without moving them.
+/// # Examples
+/// ```rust
+/// use generic_functions::sum_vec;
+/// let v = vec![1, 2, 3];
+/// assert_eq!(sum_vec(&v), 6);
+/// ```
+pub fn sum_vec<T>(v: &Vec<T>) -> T
+where
+    T: num_traits::identities::Zero + std::ops::Add + Copy,
+{
+    v.iter()
+        .fold(T::zero(), |accumulator, &part| accumulator + part)
 }
---
+++ projects/generics/generic-functions/tests/integration_test.rs
@@ -0,0 +1,38 @@
+#[test]
+fn it_l2_norm() {
+    use generic_functions::l2_norm;
+
+    let (x, y): (f32, f32) = (3., 4.);
+    assert_eq!(l2_norm(x, y), 5.);
+}
+
+#[test]
+fn it_sigmoid() {
+    use generic_functions::sigmoid;
+
+    let x = 0.0f32;
+    let y = sigmoid(x);
+    assert!((y - 0.5).abs() < 1e-6);
+
+    let x = 0.0f64;
+    let y = sigmoid(x);
+    assert!((y - 0.5).abs() < 1e-6);
+}
+
+#[test]
+fn it_vec_sum() {
+    use generic_functions::sum_vec;
+
+    let v_i32 = vec![-1, 0, 1, 2, 3];
+    assert_eq!(sum_vec(&v_i32), 5);
+    assert_eq!(sum_vec(&v_i32), 5);
+
+    let v_usize: Vec<usize> = vec![0, 1, 2, 3];
+    assert_eq!(sum_vec(&v_usize), 6);
+
+    let v_f32: Vec<f32> = vec![1., 2., 3.];
+    assert_eq!(sum_vec(&v_f32), 6_f32);
+
+    let v_f64 = vec![1., 2., 3.];
+    assert_eq!(sum_vec(&v_f64), 6.);
+}
