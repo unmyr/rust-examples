eb58126e02882b717e811781fd0ac1df5c3c846d
unmyr <56982541+unmyr@users.noreply.github.com>
2026-01-04 05:06:30 +0900

    feat(ml/xor_regression): implement XOR neural network with forward/back propagation from scratch

---
+++ projects/ml/xor_regression/src/bin/xor_reg_scratch.rs
@@ -0,0 +1,224 @@
+// use ndarray::IndexLonger;
+use num_traits::Float;
+
+use rand::Rng;
+#[allow(unused)]
+#[derive(Debug)]
+enum Activation {
+    Sigmoid,
+    Tanh,
+}
+
+impl std::fmt::Display for Activation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Activation::Sigmoid => write!(f, "sigmoid"),
+            Activation::Tanh => write!(f, "tanh"),
+        }
+    }
+}
+
+// The domain is [0, 1]
+fn sigmoid<T: Float>(x: T) -> T {
+    T::one() / (T::one() + (-x).exp())
+}
+
+// fn sigmoid_derivative<T: Float>(x: T) -> T {
+//     let s = sigmoid(x);
+//     s * (T::one() - s)
+// }
+
+// Calculated from the output of the activation function
+fn sigmoid_derivative_from_output<T: Float>(s: T) -> T {
+    s * (T::one() - s)
+}
+
+fn xor_continuous<T: Float>(x1: T, x2: T) -> T {
+    x1 + x2 - T::from(2.0).unwrap() * x1 * x2
+}
+
+// The domain is [-1,1]
+fn tanh<T: Float>(x: T) -> T {
+    x.tanh()
+}
+
+// fn tanh_derivative<T: Float>(x: T) -> T {
+//     let t = x.tanh();
+//     T::one() - t * t
+// }
+
+// Calculated from the output of the activation function
+fn tanh_derivative_from_output<T: Float>(t: T) -> T {
+    T::one() - t * t
+}
+
+fn forward<T: Float + 'static>(
+    function: &Activation,
+    input: &ndarray::ArrayBase<ndarray::ViewRepr<&T>, ndarray::Dim<[usize; 2]>>,
+    h1: &ndarray::ArrayBase<ndarray::OwnedRepr<T>, ndarray::Dim<[usize; 2]>>,
+    bias1: &ndarray::ArrayBase<ndarray::OwnedRepr<T>, ndarray::Dim<[usize; 2]>>,
+    h2: &ndarray::ArrayBase<ndarray::OwnedRepr<T>, ndarray::Dim<[usize; 2]>>,
+    bias2: &ndarray::ArrayBase<ndarray::OwnedRepr<T>, ndarray::Dim<[usize; 2]>>,
+) -> (
+    ndarray::Array2<T>,
+    ndarray::Array2<T>,
+    ndarray::Array2<T>,
+    ndarray::Array2<T>,
+) {
+    match function {
+        Activation::Sigmoid => {
+            let h1_out = h1.dot(input) + bias1;
+            let h1_out_s = h1_out.mapv(sigmoid);
+            let h2_out = h2.dot(&h1_out_s) + bias2;
+            let h2_out_s = h2_out.mapv(sigmoid);
+            (h1_out, h1_out_s, h2_out, h2_out_s)
+        }
+        Activation::Tanh => {
+            let h1_out = h1.dot(input) + bias1;
+            let h1_out_s = h1_out.mapv(tanh);
+            let h2_out = h2.dot(&h1_out_s) + bias2;
+            let h2_out_s = h2_out.mapv(sigmoid);
+            (h1_out, h1_out_s, h2_out, h2_out_s)
+        }
+    }
+}
+
+fn main() {
+    let mut rng = rand::rng();
+    let activation = Activation::Tanh;
+    let init_random_value = true;
+    // let activation = Activation::Sigmoid;
+
+    // Test predictions
+    let n_samples = 20000;
+    let mut h1;
+    let mut h2;
+    let mut bias1;
+    let mut bias2;
+    if init_random_value {
+        h1 = ndarray::Array2::from_shape_fn((2, 2), |_| rng.random_range(-0.5..0.5));
+        h2 = ndarray::Array2::from_shape_fn((1, 2), |_| rng.random_range(-0.5..0.5));
+        bias1 = ndarray::Array2::from_shape_fn((2, 1), |_| rng.random_range(-0.5..0.5));
+        bias2 = ndarray::Array2::from_shape_fn((1, 1), |_| rng.random_range(-0.5..0.5));
+    } else {
+        h1 = ndarray::arr2::<f64, 2>(&[[0.1, 0.2], [0.3, 0.4]]);
+        h2 = ndarray::arr2::<f64, 2>(&[[0.5, 0.6]]);
+        bias1 = ndarray::arr2::<f64, 1>(&[[0.1], [0.1]]);
+        bias2 = ndarray::arr2::<f64, 1>(&[[0.1]]);
+    }
+
+    let learning_rate = 0.5;
+
+    let test_inputs = ndarray::arr2(&[[0., 0.], [0., 1.], [1., 0.], [1., 1.]]);
+    let test_answers = ndarray::arr2(&[[0., 1., 1., 0.]]);
+    let mini_batch_size = test_inputs.shape()[0];
+    println!(
+        "learning_rate={}, n_samples={}, mini_batch_size={}, activation={:?}",
+        learning_rate, n_samples, mini_batch_size, activation
+    );
+    for n in 0..n_samples {
+        let mut h2s_outputs = ndarray::Array2::<f64>::zeros((1, mini_batch_size));
+
+        let mut delta_h2_list = ndarray::Array2::<f64>::zeros((1, mini_batch_size));
+        let mut delta_h1_list = ndarray::Array2::<f64>::zeros((2, mini_batch_size));
+
+        // Accumulate the gradient for each sample
+        let mut grad_h2 = ndarray::Array2::zeros(h2.dim());
+        let mut grad_h1 = ndarray::Array2::zeros(h1.dim());
+        let mut delta_h1_total = ndarray::Array2::<f64>::zeros((2, 1));
+        let mut delta_h2_total = ndarray::Array2::<f64>::zeros((1, 1));
+        for (i, in_view) in test_inputs.rows().into_iter().enumerate() {
+            let in_col_v = in_view.insert_axis(ndarray::Axis(1));
+            let (x1, x2) = (in_col_v[[0, 0]], in_col_v[[1, 0]]);
+            // let (x1, x2) = (rng.random_range(0.0..=1.0), rng.random_range(0.0..=1.0));
+            let y_train = ndarray::arr2::<f64, 1>(&[[xor_continuous(x1, x2)]]);
+            // println!("(x1,y1)=({:.0},{:.0}); y_train={:.0}", x1, x2, y_train);
+            let (_h1_out, h1_out_s, _h2_out, h2_out_s) =
+                forward::<f64>(&activation, &in_col_v.view(), &h1, &bias1, &h2, &bias2);
+            let mut h2s_output_column = h2s_outputs.column_mut(i);
+            h2s_output_column.assign(&h2_out_s.column(0).view());
+
+            let output_error = &h2_out_s - &y_train;
+            let delta_h2 = &output_error * &h2_out_s.mapv(sigmoid_derivative_from_output);
+            let mut delta_h2_column = delta_h2_list.column_mut(i);
+            delta_h2_column.assign(&delta_h2.column(0).view());
+
+            // println!("h1_out_s={}", h1_out_s);
+            // println!("delta_h2={}", delta_h2);
+            let mut delta_h2_total_work = delta_h2_total.column_mut(0);
+            let delta_sum_h2 = &delta_h2_total_work.view() + &delta_h2.column(0).view();
+            delta_h2_total_work.assign(&delta_sum_h2.view());
+
+            // println!("in_col_v={}", in_col_v);
+            let delta_h1 = match activation {
+                Activation::Sigmoid => {
+                    h2.t().dot(&delta_h2) * h1_out_s.mapv(sigmoid_derivative_from_output)
+                }
+                Activation::Tanh => {
+                    h2.t().dot(&delta_h2) * h1_out_s.mapv(tanh_derivative_from_output)
+                }
+            };
+            // println!("delta_h1={}", delta_h1);
+
+            // Calculate gradients in a loop (using cross products)
+            grad_h2 += &delta_h2.dot(&h1_out_s.t());
+            grad_h1 += &delta_h1.dot(&in_col_v.t());
+
+            let mut delta_h1_column = delta_h1_list.column_mut(i);
+            delta_h1_column.assign(&delta_h1.column(0).view());
+            let mut delta_h1_total_work = delta_h1_total.column_mut(0);
+            let delta_sum_h1 = &delta_h1_total_work.view() + &delta_h1.column(0).view();
+            delta_h1_total_work.assign(&delta_sum_h1.view());
+        }
+        h2 = &h2 - &grad_h2 * learning_rate / (mini_batch_size as f64);
+        bias2 = &bias2 - learning_rate * &delta_h2_total / (mini_batch_size as f64);
+        // println!("h2={:.4} bias2={:.4}", h2, bias2);
+
+        h1 = &h1 - &grad_h1 * learning_rate / (mini_batch_size as f64);
+        bias1 = &bias1 - learning_rate * &delta_h1_total / (mini_batch_size as f64);
+        // println!("h1={:.4} bias1={:.4}", h1, bias1);
+
+        let loss = (&h2s_outputs - &test_answers).powf(2.).sum() / (mini_batch_size as f64);
+        if n == 0 || n % 1000 == 999 {
+            println!(
+                "[{:05}]: loss={:.4} delta_h1_total^T={:.4}, delta_h2_total={:.4}",
+                n + 1,
+                loss,
+                delta_h1_total.t(),
+                delta_h2_total
+            );
+        }
+    }
+    // println!(
+    //     "Trained weights: h1={:.4}, bias1: {:.4}, h2={:.4}, bias2: {:.4}",
+    //     h1, bias1, h2, bias2
+    // );
+    println!(
+        "learning_rate={}, n_samples={}, mini_batch_size={}, activation={:?}",
+        learning_rate, n_samples, mini_batch_size, activation
+    );
+    println!("== XOR Predictions ==");
+    let mut correct_counts = 0;
+    for in_view in test_inputs.rows() {
+        let in_col_v = in_view.insert_axis(ndarray::Axis(1));
+        let (x1, x2) = (in_col_v[[0, 0]], in_col_v[[1, 0]]);
+        let (_h1_out, _h1_out_s, _h2_out, h2_out_s) =
+            forward(&activation, &in_col_v.view(), &h1, &bias1, &h2, &bias2);
+        let ans11 = ndarray::arr2(&[[xor_continuous(x1, x2)]]);
+        let loss = (&ans11 - &h2_out_s).powf(2.).sum() / 2.;
+        if loss < 0.05 {
+            correct_counts += 1;
+        }
+        println!(
+            "Input: [{:?}] => Predicted: {:.2}, answer: {:.0}, loss: {:.2}",
+            [x1, x2],
+            h2_out_s[[0, 0]],
+            ans11[[0, 0]],
+            loss
+        );
+    }
+    println!(
+        "Accuracy: {:.2}%",
+        (correct_counts as f64 / (test_inputs.shape()[0]) as f64) * 100.0
+    );
+}
